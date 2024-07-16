e87d2bb29ffa847679fff330f618416b8b18004f
Михаил Киржаков <kirzhakovma@mail.ru>
2024-07-16 23:04:29 +0300

    добавлены версия api в параметр routes, добавлены роуты reviews и comments

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,15 +2,23 @@ from rest_framework import routers
 
 from django.urls import include, path
 
-from .views import CategoriesViewSet, GenresViewSet, TitlesViewSet
+from .views import (CategoriesViewSet, GenresViewSet, TitlesViewSet,
+                    ReviewsViewSet, CommentsViewSet)
 
 
-router = routers.DefaultRouter()
-router.register('categories', CategoriesViewSet)
-router.register('genres', GenresViewSet)
-router.register('titles', TitlesViewSet)
+v1_router = routers.DefaultRouter()
+v1_router.register('categories', CategoriesViewSet)
+v1_router.register('genres', GenresViewSet)
+v1_router.register('titles', TitlesViewSet)
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews', ReviewsViewSet, basename='reviews'
+)
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentsViewSet, basename='comments'
+)
 
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(v1_router.urls)),
 ]
