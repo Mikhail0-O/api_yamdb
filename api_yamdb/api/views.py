f6e598b386b43b201be09feac04d62f749906fe9
Михаил Киржаков <kirzhakovma@mail.ru>
2024-07-16 23:03:34 +0300

    Добавлены ReviewsViewSet и CommentsViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,10 +1,14 @@
-from rest_framework import viewsets, permissions, filters, mixins
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, mixins, permissions, serializers, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
-from reviews.models import Categories, Genres, Titles
+from .permissions import IsAuthorOrAdminOrModerator
+from reviews.models import Categories, Comments, Genres, Reviews, Titles
 from .serializers import (CategoriesSerializer,
+                          CommentsSerializer,
                           GenresSerializer,
-                          TitlesSerializer)
+                          TitlesSerializer,
+                          ReviewsSerializer)
 
 
 class CategoriesViewSet(mixins.ListModelMixin,
@@ -18,10 +22,12 @@ class CategoriesViewSet(mixins.ListModelMixin,
     search_fields = ('name',)
 
 
-class GenresViewSet(mixins.ListModelMixin,
-                    mixins.CreateModelMixin,
-                    mixins.DestroyModelMixin,
-                    viewsets.GenericViewSet):
+class GenresViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = Genres.objects.all()
     serializer_class = GenresSerializer
     pagination_class = LimitOffsetPagination
@@ -35,3 +41,33 @@ class TitlesViewSet(viewsets.ModelViewSet):
     pagination_class = LimitOffsetPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'year', 'genre', 'category')
+
+
+class ReviewsViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewsSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_title(self):
+        return get_object_or_404(Titles, id=self.kwargs['title_id'])
+
+    def perform_create(self, serializer):
+        author = self.request.user
+        if Reviews.objects.filter(title=self.get_title(), author=author).exists():
+            raise serializers.ValidationError('Вы уже оставили отзыв!')
+        serializer.save(author=author)
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentsSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly, IsAuthorOrAdminOrModerator
+    )
+
+    def get_review(self):
+        return get_object_or_404(Reviews, id=self.kwargs['review_id'])
+
+    def get_queryset(self):
+        return Comments.objects.filter(review=self.get_review())
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, review_id=self.get_review())
