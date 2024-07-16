4cebaa5c1a39a4fd45c94d80a5631add75bd1edc
Михаил Киржаков <kirzhakovma@mail.ru>
2024-07-16 23:05:24 +0300

    Добавлены сериализаторы Reviews и Comments, добавлен вывод среднего рейтинга в серилизаторе Titles

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
+from django.db.models import Avg
 from rest_framework import serializers
 
-from reviews.models import Categories, Genres, Titles
+from reviews.models import Categories, Comments, Genres, Reviews, Titles
 
 
 class CategoriesSerializer(serializers.ModelSerializer):
@@ -17,8 +18,32 @@ class GenresSerializer(serializers.ModelSerializer):
         fields = ('name', 'slug')
 
 
+class ReviewsSerializer(serializers.ModelSerializer):
+    author_username = serializers.CharField(
+        source='author.username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Reviews
+        fields = (
+            'id', 'text', 'rating', 'author_username', 'title', 'pub_date'
+        )
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    author_username = serializers.CharField(
+        source='author.username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comments
+        fields = ('id', 'text', 'author_username', 'review', 'pub_date')
+
+
 class TitlesSerializer(serializers.ModelSerializer):
-    rating = 'нужно написать!)'
+    rating = serializers.SerializerMethodField()
     genre = serializers.SlugRelatedField(
         many=True,
         slug_field='slug',
@@ -29,9 +54,17 @@ class TitlesSerializer(serializers.ModelSerializer):
         slug_field='slug',
         queryset=Categories.objects.all()
     )
+    reviews = ReviewsSerializer(many=True, read_only=True)
+    comments = CommentsSerializer(many=True, read_only=True)
+
+    def get_rating(self, obj):
+        reviews = obj.reviews.all()
+        if reviews.exists():
+            return reviews.aggregate(Avg('rating'))['rating__avg']
 
     class Meta:
         model = Titles
         fields = (
-            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+            'id', 'name', 'year', 'rating', 'description',
+            'genre', 'category', 'reviews', 'comments'
         )
