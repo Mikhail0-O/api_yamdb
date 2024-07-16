741fe308d577a8bc1d8e9059c296085f10efe833
Ilya_Pryzhykau <tuarone@gmail.com>
2024-07-16 20:41:32 +0200

    докинул начало админки

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categories, Genres, Titles
+
+
+class TitlesAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+    )
+    search_fields = ('name', 'year', 'rating', 'genre', 'category')
+
+
+admin.site.register(Titles, TitlesAdmin)
+admin.site.register(Genres)
+admin.site.register(Categories)
