2bf7b67c97f5fdb95ac7317853cb8a24da3274be
Михаил Киржаков <kirzhakovma@mail.ru>
2024-07-16 19:34:09 +0300

    Добавил модели Reviews, Comments. В модели Reviews применил параметр choices для всплывающего окна выбора рейтинга

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+
+from users.models import CustomUser
 
 
 class Categories(models.Model):
@@ -60,3 +61,54 @@ class Titles(models.Model):
     def save(self, *args, **kwargs):
         self.clean()  # Вызываем валидацию перед сохранением
         super().save(*args, **kwargs)
+
+
+class Reviews(models.Model):
+    text = models.TextField('Текст отзыва')
+    score = models.IntegerField(
+        'Рейтинг',
+        choices=[(i, i) for i in range(1, 11)],  # Даем выбрать значение
+        default=None,
+        null=False,
+        blank=False
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        verbose_name='Автор отзыва',
+        on_delete=models.CASCADE
+    )
+    title = models.ForeignKey(
+        Titles,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+    def __str__(self):
+        return f'Отзыв на {self.title} от {self.author}'
+
+
+class Comments(models.Model):
+    text = models.TextField('Текст комментария')
+    author = models.ForeignKey(
+        CustomUser,
+        verbose_name='Автор комментария',
+        on_delete=models.CASCADE
+    )
+    review = models.ForeignKey(
+        Reviews,
+        verbose_name='Отзыв',
+        on_delete=models.CASCADE
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'Комментарий к отзыву {self.review} от {self.author}'
