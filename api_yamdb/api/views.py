711d4b9a444cfa2ed3001d747d4c69f960d5c0cf
Михаил Киржаков <kirzhakovma@mail.ru>
2024-07-17 19:12:51 +0300

    Подключил миксины

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, serializers, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
+from .mixins import GetTitleMixin, GetReviewMixin
 from reviews.models import Categories, Comments, Genres, Reviews, Titles
 from .serializers import (CategoriesSerializer,
                           CommentsSerializer,
@@ -42,27 +43,23 @@ class TitlesViewSet(viewsets.ModelViewSet):
     search_fields = ('name', 'year', 'genre', 'category')
 
 
-class ReviewsViewSet(viewsets.ModelViewSet):
+class ReviewsViewSet(GetTitleMixin, viewsets.ModelViewSet):
     serializer_class = ReviewsSerializer
-
-    def get_title(self):
-        return get_object_or_404(Titles, id=self.kwargs['title_id'])
 
     def perform_create(self, serializer):
         author = self.request.user
-        if Reviews.objects.filter(title=self.get_title(), author=author).exists():
+        if Reviews.objects.filter(
+            title=self.get_title(), author=author
+        ).exists():
             raise serializers.ValidationError('Вы уже оставили отзыв!')
         serializer.save(author=author)
 
 
-class CommentsViewSet(viewsets.ModelViewSet):
+class CommentsViewSet(GetReviewMixin, viewsets.ModelViewSet):
     serializer_class = CommentsSerializer
-
-    def get_review(self):
-        return get_object_or_404(Reviews, id=self.kwargs['review_id'])
 
     def get_queryset(self):
         return Comments.objects.filter(review=self.get_review())
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user, review_id=self.get_review())
+        serializer.save(author=self.request.user, review=self.get_review())
