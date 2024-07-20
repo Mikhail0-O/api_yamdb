fc68cef1839fd2f9728cc3b6157045837c87896b
Mikhail Zhukov <M-kh-l.i-ai-O@yandex.ru>
2024-07-20 12:45:43 +0300

    Добавил в api/urls.py маршрут для работы с пользователями

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,12 +3,14 @@ from rest_framework import routers
 from django.urls import include, path
 
 from .views import CategoriesViewSet, GenresViewSet, TitlesViewSet
+from users.views import UserViewSet
 
 
 router = routers.DefaultRouter()
 router.register('categories', CategoriesViewSet)
 router.register('genres', GenresViewSet)
 router.register('titles', TitlesViewSet)
+router.register('users', UserViewSet)
 
 app_name = 'api'
 
