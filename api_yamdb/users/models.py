f5d71b6474f2213203dfe7d140db39717583ba52
Mikhail Zhukov <M-kh-l.i-ai-O@yandex.ru>
2024-07-20 12:36:02 +0300

    Настроил users/models.py для кастомного пользователя

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,14 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator, MinLengthValidator
 
-
-def validate_role(value):
-    if value not in ['user', 'admin', 'moderator']:
-        raise ValidationError(f"{value} - недопустимая роль")
+from validators import validate_role, validate_username
 
 
 class CustomUser(AbstractUser):
+    username = models.CharField(
+        'username', max_length=150, unique=True,
+        validators=[validate_username,
+                    MinLengthValidator(limit_value=5),
+                    RegexValidator(regex=r'^[\w.@+-]+\Z')]
+    )
+    email = models.EmailField(
+        'email',
+        unique=True, max_length=254)
     bio = models.TextField('Биография', blank=True)
     role = models.CharField(
         max_length=50, blank=True, validators=[validate_role],
