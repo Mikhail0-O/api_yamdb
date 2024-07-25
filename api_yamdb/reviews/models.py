7ff5f41738ebd39472310b2441530edf00d2ff05
Mihail Kirzhakov <kirzhakovma@mail.ru>
2024-07-25 13:20:43 +0300

    Созданы 3 абстрактные модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -6,9 +6,8 @@ from users.models import CustomUser
 from api.validators import year_validator
 
 
-class NameSlugModel(models.Model):
+class NameModel(models.Model):
     name = models.CharField('Название', max_length=256)
-    slug = models.SlugField('Идентификатор', max_length=50, unique=True)
 
     class Meta:
         abstract = True
@@ -17,22 +16,43 @@ class NameSlugModel(models.Model):
         return self.name
 
 
-class Category(NameSlugModel):
+class SlugModel(models.Model):
+    slug = models.SlugField('Идентификатор', max_length=50, unique=True)
+
+    class Meta:
+        abstract = True
+
+
+class TextAuthorPubDate(models.Model):
+    text = models.TextField('Текст')
+    author = models.ForeignKey(
+        CustomUser,
+        verbose_name='Автор',
+        on_delete=models.CASCADE
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(NameModel, SlugModel):
+
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'Категории'
         ordering = ['id']
 
 
-class Genre(NameSlugModel):
+class Genre(NameModel, SlugModel):
+
     class Meta:
         verbose_name = 'жанр'
         verbose_name_plural = 'Жанры'
         ordering = ['id']
 
 
-class Title(models.Model):
-    name = models.CharField('Название произведения', max_length=256)
+class Title(NameModel):
     year = models.IntegerField('Год выхода', validators=[year_validator])
     description = models.TextField('Описание произведения', blank=True)
     genre = models.ManyToManyField(
@@ -51,12 +71,8 @@ class Title(models.Model):
         verbose_name_plural = 'Произведения'
         ordering = ['id']
 
-    def __str__(self):
-        return self.name
 
-
-class Review(models.Model):
-    text = models.TextField('Текст отзыва')
+class Review(TextAuthorPubDate):
     score = models.PositiveIntegerField(
         verbose_name='Рейтинг',
         validators=[
@@ -68,19 +84,12 @@ class Review(models.Model):
             )
         ]
     )
-    author = models.ForeignKey(
-        CustomUser,
-        related_name='reviews',
-        verbose_name='Автор отзыва',
-        on_delete=models.CASCADE,
-    )
     title = models.ForeignKey(
         Title,
         related_name='reviews',
         verbose_name='Произведение',
         on_delete=models.CASCADE,
     )
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
 
     class Meta:
         verbose_name = 'отзыв'
@@ -97,13 +106,7 @@ class Review(models.Model):
         return f'Отзыв на {self.title} от {self.author}'
 
 
-class Comment(models.Model):
-    text = models.TextField('Текст комментария', max_length=500)
-    author = models.ForeignKey(
-        CustomUser,
-        verbose_name='Автор комментария',
-        on_delete=models.CASCADE
-    )
+class Comment(TextAuthorPubDate):
     review = models.ForeignKey(
         Review,
         verbose_name='Отзыв',
@@ -111,7 +114,6 @@ class Comment(models.Model):
         blank=True,
         null=True
     )
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
 
     class Meta:
         verbose_name = 'Комментарий'
