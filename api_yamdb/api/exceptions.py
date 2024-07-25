0ba0e814d7ae19a02deb90bc7edd13934a0d999b
Mikhail Zhukov <M-kh-l.i-ai-O@yandex.ru>
2024-07-25 19:38:19 +0300

    Рефакторинг

--- api_yamdb/api/exceptions.py
+++ api_yamdb/api/exceptions.py
@@ -5,7 +5,7 @@ from rest_framework import status
 
 class CustomValidation(APIException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    default_detail = 'A server error occurred.'
+    default_detail = 'Произошла ошибка сервера.'
 
     def __init__(self, detail, field, status_code):
         if status_code is not None:
